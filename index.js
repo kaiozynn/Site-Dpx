96cb892388f8f421444997f6bd8b94c325f1a866
Caio Ferreira <cs1919328@gmail.com>
2023-12-25 14:29:05 -0300

    Servidor

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ appExpress.get('/seletiva', (req, res) => {
 
 //Dados vindos do formulário são tratados e enviado pra o firebase aqui do cadastro
 
-appExpress.post('/cad', async function (req, res) {
+appExpress.post('/cadidatar', async function (req, res) {
   const name = req.body.nick;
   const id_players = req.body.id;
   const elo = req.body.elo;
